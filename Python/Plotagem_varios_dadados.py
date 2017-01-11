64dca15bf687bbb0d494f7f376e0e63d86f1d90d
Italo Fernandes <italogsfernandes@gmail.com>
2017-01-11 02:10:50 -0200

    aula ronaldo

--- Python/Plotagem_varios_dadados.py
+++ Python/Plotagem_varios_dadados.py
@@ -20,26 +20,30 @@ def makeFig(): 										#desenhadora
 	plt.title('Dados do sensor')      				
 	plt.grid(True)                                  
 	plt.ylabel('Lido')                        	    #Set ylabels
-	plt.plot(ACx, 'ro-', label='X')
+	plt.plot(ACx, 'bo-', label='X')
 	plt.legend(loc='upper left')    
 
-	plt2=plt.twinx()                                #Criando um segundo eixo y
-	plt2.plot(ACy, 'bo-', label='Y')
+	plt2=plt.twinx()                      #Criando um segundo eixo y
+	plt2.plot(ACy, 'go-', label='Y')
 	plt2.ticklabel_format(useOffset=False)
 	plt2.legend(loc='upper right')
     
-	plt3=plt.twinx()                                #Criando um terceiro eixo y
-	plt3.plot(ACz, 'go-', label='Z')
+	plt3=plt.twinx()                      #Criando um terceiro eixo z
+	plt3.plot(ACz, 'yo-', label='Z')
 	plt3.ticklabel_format(useOffset=False)
-	plt3.legend(loc='downer right')
-    
+	plt3.legend(loc='lower left')
+	
+	plt4=plt.twinx()
+	plt4.plot(ACz, 'rx-', label='T')
+	plt4.ticklabel_format(useOffset=False)
+	plt4.legend(loc='center right')
 
 while True: #Uhuuulllll
     while (arduinoData.inWaiting()==0):			 #espera ter dado pra ler
         pass
     arduinoString = arduinoData.readline()
     dataArray = arduinoString.split(',')
-    print(dataArray)
+    
     if len(dataArray)>6: 
 		ACx.append(float( dataArray[0])) 			#convertendo dados lidos e adicionando as listas
 		ACy.append(float( dataArray[1]))
@@ -49,8 +53,10 @@ while True: #Uhuuulllll
 		Giry.append(float( dataArray[5]))
 		Girz.append(float( dataArray[6]))
 		
+		print(dataArray)
+		
 		drawnow(makeFig)                      
-		plt.pause(.000001)                     
+		#plt.pause(0.0000001)                     
 		cnt=cnt+1    								#contagem de pontos no grafico
     if(cnt>100):                            		#se tem mais de 50 apagar o mais antigo
     	ACx.pop(0)
